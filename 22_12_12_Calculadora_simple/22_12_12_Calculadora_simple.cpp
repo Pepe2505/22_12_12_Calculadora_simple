12faf36042f31b99e21555f373cc5451f7d85994
Pepe2505 <pepe8alzojeda@gmail.com>
2022-12-12 11:22:20 -0600

    Calculadora simple
    Se cambio completamente el codigo

--- 22_12_12_Calculadora_simple/22_12_12_Calculadora_simple.cpp
+++ 22_12_12_Calculadora_simple/22_12_12_Calculadora_simple.cpp
@@ -1,19 +1,66 @@
-// 22_12_12_Calculadora_simple.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// 22_10_24_005_calculadora_simple.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// Jose Antonio Ochoa Lopez
+// en esta pratica vamos hacer una calculadora
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "hola World!\n";
+	// declarar las variantes
+	int operacion;
+	int numero_1_a_sumar = 0;
+	int numero_2_a_sumar = 0;
+	int numero_1_a_restar = 0;
+	int numero_2_a_restar = 0;
+	int numero_1_a_multiplipar = 0;
+	int numero_2_a_multiplicar = 0;
+	int numero_1_a_dividir = 0;
+	int numero_2_a_dividir = 0;
+	int resultado_de_la_suma = 0;
+	int resultado_de_la_resta = 0;
+	int resultado_de_la_multiplicacion = 0;
+	int resultado_de_la_division = 0;
+
+	// preguntarle al usuario que operacion quieres hacer
+	std::cout << "cual es la operacion que deseas hacer\n";
+	std::cout << "para eligir la operacion que desea tendras que escoger entre el 1-4 (1 es la suma), (2 es la resta), (3 es la mutiplicacion) y (4 es divsion)\n";
+	std::cin >> operacion;
+
+	//pedir al usuario que ingrese la cantidad
+	switch (operacion)
+	{
+	case 1:
+		std::cout << "ingrese los 2 numeros que quiere sumar\n";
+		std::cin >> numero_1_a_sumar;
+		std::cin >> numero_2_a_sumar;
+		resultado_de_la_suma = numero_1_a_sumar + numero_2_a_sumar;
+		std::cout << "el resultado de la suma es " << resultado_de_la_suma << std::endl;
+		break;
+
+	case 2:
+		std::cout << "ingrese las 2 cantidades que quiere resta\n";
+		std::cin >> numero_1_a_restar;
+		std::cin >> numero_2_a_restar;
+		resultado_de_la_resta = numero_1_a_restar - numero_2_a_restar;
+		std::cout << "el resultado de la resta es " << resultado_de_la_resta << std::endl;
+		break;
+
+	case 3:
+		std::cout << "que cantidades quieres multiplicar\n";
+		std::cin >> numero_1_a_multiplipar;
+		std::cin >> numero_2_a_multiplicar;
+		resultado_de_la_multiplicacion = numero_1_a_multiplipar * numero_2_a_multiplicar,
+			std::cout << "el resultado de la multiplicacion es " << resultado_de_la_multiplicacion << std::endl;
+		break;
+
+	case 4:
+		std::cout << "cual es el numero que quieres dividir\n";
+		std::cin >> numero_1_a_dividir;
+		std::cout << "entre que lo quieres dividir\n";
+		std::cin >> numero_2_a_dividir;
+		resultado_de_la_division = numero_1_a_dividir / numero_2_a_dividir;
+		std::cout << "el resultado de la division " << resultado_de_la_division << std::endl;
+		break;
+	}
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
